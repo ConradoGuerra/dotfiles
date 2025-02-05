d78c31f101f80f0691a0d9730eafcdca76383744
conrado.guerra <conrado.guerra@ilek.fr>
2025-02-05 21:35:11 +0100

    Update packages conig, add catppuccin

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,8 +6,38 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
+	keys = {
+		{ "<leader>b", "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
+		{ "<leader>e", "<cmd>Neotree reveal<cr>", desc = "Reveal Explorer" },
+	},
 	config = function()
-		vim.keymap.set("n", "<C-b>", ":Neotree reveal left toggle<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+		require("neo-tree").setup({
+			close_if_last_window = true, -- Close NeoTree if it's the last open window
+			popup_border_style = "rounded",
+			enable_git_status = true, -- Show Git status indicators
+			enable_diagnostics = true, -- Show diagnostic indicators (e.g., LSP errors)
+			default_component_configs = {
+				indent = {
+					indent_size = 2,
+					padding = 1,
+					with_markers = true,
+					with_expanders = true,
+					expander_collapsed = "",
+					expander_expanded = "",
+					context_marker = "",
+				},
+			},
+			filesystem = {
+				filtered_items = {
+					visible = true, -- Hide hidden files by default
+				},
+				follow_current_file = true, -- Keep the current file centered in the tree
+				use_libuv_file_watcher = true,
+			},
+			window = {
+				position = "left", -- Position of the NeoTree window
+				width = 30, -- Width of the NeoTree window
+			},
+		})
 	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
+			ensure_installed = { "lua", "go", "python", "javascript", "typescript", "json", "markdown", "bash" }, -- List of languages to install parsers for
 			auto_install = true,
 			sync_install = false,
 			highlight = { enable = true },
