d6bc98c1c18c90bcd54d3b21e986189940c52f0e
conrado.guerra <conrado.guerra@ilek.fr>
2025-02-09 20:27:04 +0100

    Add blank line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,23 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("lualine").setup()
+    require("lualine").setup({
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = {
+          { "filename", path = 1 }, -- Show relative path + filename
+        },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      -- Optional: Show path in inactive windows too
+      inactive_sections = {
+        lualine_c = {
+          { "filename", path = 1 }, -- Relative path in inactive buffers
+        },
+      },
+    })
   end,
 }
