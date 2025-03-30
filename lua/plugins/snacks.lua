879d4171a42837c10ddca21334e018b938c44dab
conrado.guerra <conrado.guerra@ilek.fr>
2025-03-30 20:25:20 +0200

    Change layout of searchs (LSPs, layouts)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -16,8 +16,27 @@ return {
 			expand = true,
 		},
 		notifier = { enabled = true },
-		picker = { enabled = true },
-		lazygit = {},
+		picker = {
+			enabled = true,
+			layout = {
+				layout = {
+					backdrop = false,
+					box = "vertical",
+					width = 0.9, -- 90% of the editor width
+					height = 0.95, -- 95% of the editor height
+					min_width = 120,
+					{
+						box = "vertical",
+						border = "rounded",
+						title = "{title} {live} {flags}",
+						{ win = "input", height = 1, border = "bottom" },
+						{ win = "list", border = "none", height = 10 },
+					},
+					{ win = "preview", title = "{preview}", border = "rounded" },
+				},
+			},
+		},
+		lazygit = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
 		scroll = { enabled = true },
@@ -381,92 +400,6 @@ return {
 			desc = "Colorschemes",
 		},
 		-- LSP
-		{
-			"gd",
-			function()
-				Snacks.picker.lsp_definitions()
-			end,
-			desc = "Goto Definition",
-		},
-		{
-			"gD",
-			function()
-				Snacks.picker.lsp_declarations()
-			end,
-			desc = "Goto Declaration",
-		},
-		{
-			"gr",
-			function()
-				Snacks.picker.lsp_references()
-			end,
-			nowait = true,
-			desc = "References",
-		},
-		{
-			"gI",
-			function()
-				Snacks.picker.lsp_implementations()
-			end,
-			desc = "Goto Implementation",
-		},
-		{
-			"gy",
-			function()
-				Snacks.picker.lsp_type_definitions()
-			end,
-			desc = "Goto T[y]pe Definition",
-		},
-		{
-			"<leader>ss",
-			function()
-				Snacks.picker.lsp_symbols()
-			end,
-			desc = "LSP Symbols",
-		},
-		{
-			"<leader>sS",
-			function()
-				Snacks.picker.lsp_workspace_symbols()
-			end,
-			desc = "LSP Workspace Symbols",
-		},
-		-- Other
-		{
-			"<leader>z",
-			function()
-				Snacks.zen()
-			end,
-			desc = "Toggle Zen Mode",
-		},
-		{
-			"<leader>Z",
-			function()
-				Snacks.zen.zoom()
-			end,
-			desc = "Toggle Zoom",
-		},
-		{
-			"<leader>.",
-			function()
-				Snacks.scratch()
-			end,
-			desc = "Toggle Scratch Buffer",
-		},
-		{
-			"<leader>S",
-			function()
-				Snacks.scratch.select()
-			end,
-			desc = "Select Scratch Buffer",
-		},
-		{
-			"<leader>n",
-			function()
-				Snacks.notifier.show_history()
-			end,
-			desc = "Notification History",
-		},
 		{
 			"<leader>bd",
 			function()
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,34 @@ return {
 	},
 	config = function()
 		local telescope = require("telescope")
-
+		telescope.setup({
+			defaults = {
+				layout_strategy = "vertical",
+				layout_config = {
+					vertical = {
+						width = 0.9, -- 90% of the editor width
+						height = 0.95, -- 95% of the editor height
+						preview_height = 0.5, -- 50% of the height for preview
+						prompt_position = "top", -- input at the top
+					},
+				},
+				path_display = { "relative" }, -- Just show relative path
+			},
+			pickers = {
+				lsp_references = {
+					layout_config = {
+						vertical = {
+							prompt_position = "top", -- Input at the top
+							preview_height = 0.5, -- 50% for preview
+							mirror = true, -- Preview below results
+						},
+					},
+					path_display = { "relative" }, -- Just show relative path
+					show_line = false, -- Hide line numbers
+					trim_text = true, -- Remove surrounding code preview
+				},
+			},
+		})
 		telescope.load_extension("fzf")
 	end,
 }
