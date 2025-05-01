17da0c80ef1bb1e575b0992df605d39ed360db53
conrado.guerra <conrado.guerra@ilek.fr>
2025-05-01 18:29:47 +0200

    Change noice to a simpler version

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,23 +1,19 @@
 return {
 	"folke/noice.nvim",
-	opts = {
-		cmdline = {
-			enabled = true,
-			view = "cmdline_popup",
-		},
-		popupmenu = {
-			enabled = true,
-			backend = "nui",
-		},
-		messages = { enabled = false },
-		notify = { enabled = false },
-		lsp = {
-			progress = { enabled = false },
-			hover = { enabled = false },
-			signature = { enabled = false },
-		},
-		routes = {
-			{ filter = { event = "msg_show" }, opts = { skip = true } },
-		},
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
 	},
+	config = function()
+		require("noice").setup({
+			messages = {
+				enabled = true, -- enables the Noice messages UI
+				view = "notify", -- default view for messages
+				view_error = "notify", -- view for errors
+				view_warn = "notify", -- view for warnings
+				view_history = "messages", -- view for :messages
+				view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+			},
+		})
+	end,
 }
