f5a732748e7ac158122214736da463573b9b21b5
conrado.guerra <conrado.guerra@ilek.fr>
2025-05-07 08:44:29 +0200

    Add search count to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,15 +4,35 @@ return {
 	config = function()
 		local lualine = require("lualine")
 		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-		lualine.setup({
 
+		-- Custom component for search count
+		local search_count = {
+			function()
+				local search = require("noice").api.status.search.get()
+				return search and search or ""
+			end,
+			cond = function()
+				return package.loaded["noice"] and require("noice").api.status.search.has()
+			end,
+			color = { fg = "#7dcfff" }, -- Customize color as needed
+		}
+
+		-- Get noice mode (replaces the deprecated statusline.mode.get)
+		local noice_mode = {
+			function()
+				return require("noice").api.status.mode.get()
+			end,
+			cond = function()
+				return package.loaded["noice"] and require("noice").api.status.mode.has()
+			end,
+			color = { fg = "#ff9e64" },
+		}
+
+		lualine.setup({
 			sections = {
 				lualine_x = {
-					{
-						require("noice").api.statusline.mode.get,
-						cond = require("noice").api.statusline.mode.has,
-						color = { fg = "#ff9e64" },
-					},
+					search_count, -- Add search count component
+					noice_mode, -- Using the updated API
 					{
 						lazy_status.updates,
 						cond = lazy_status.has_updates,
