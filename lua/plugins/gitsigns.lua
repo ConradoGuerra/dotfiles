b9040727a2eec7366785543b663c1d90870b46e3
conrado.guerra <conrado.guerra@ilek.fr>
2025-05-21 19:25:44 +0200

    Change gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -14,34 +14,26 @@ return {
 			map("n", "[h", gs.prev_hunk, "Prev Hunk")
 
 			-- Actions
-			map("n", "<leader>ghs", gs.stage_hunk, "Stage hunk")
-			map("n", "<leader>ghr", gs.reset_hunk, "Reset hunk")
-			map("v", "<leader>ghs", function()
-				gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, "Stage hunk")
-			map("v", "<leader>ghr", function()
-				gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, "Reset hunk")
+			map("n", "<leader>gs", gs.stage_hunk, "Stage hunk")
+			map("n", "<leader>gr", gs.reset_hunk, "Reset hunk")
 
-			map("n", "<leader>ghS", gs.stage_buffer, "Stage buffer")
-			map("n", "<leader>ghR", gs.reset_buffer, "Reset buffer")
+			map("n", "<leader>gS", gs.stage_buffer, "Stage buffer")
+			map("n", "<leader>gR", gs.reset_buffer, "Reset buffer")
 
-			map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo stage hunk")
+			map("n", "<leader>gu", gs.undo_stage_hunk, "Undo stage hunk")
 
-			map("n", "<leader>ghp", gs.preview_hunk, "Preview hunk")
+			map("n", "<leader>gp", gs.preview_hunk, "Preview hunk")
 
-			map("n", "<leader>ghb", function()
+			map("n", "<leader>gb", function()
 				gs.blame_line({ full = true })
 			end, "Blame line")
-			map("n", "<leader>ghB", gs.toggle_current_line_blame, "Toggle line blame")
 
-			map("n", "<leader>ghd", gs.diffthis, "Diff this")
-			map("n", "<leader>ghD", function()
+			map("n", "<leader>gt", gs.toggle_current_line_blame, "Toggle line blame")
+
+			map("n", "<leader>gd", gs.diffthis, "Diff this")
+			map("n", "<leader>gD", function()
 				gs.diffthis("~")
 			end, "Diff this ~")
-
-			-- Text object
-			map({ "o", "x" }, "gih", ":<C-U>Gitsigns select_hunk<CR>", "Gitsigns select hunk")
 		end,
 	},
 }
