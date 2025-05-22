85cd3d9add484617a9e21f89458fe91a956d940b
conrado.guerra <conrado.guerra@ilek.fr>
2025-05-22 20:29:15 +0200

    Fix mason bug

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -3,19 +3,18 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
-		{ "antosha417/nvim-lsp-file-operations", config = true },
-		{ "folke/neodev.nvim", opts = {} },
+		{
+			"antosha417/nvim-lsp-file-operations",
+			config = true,
+		},
+		{
+			"folke/neodev.nvim",
+			opts = {},
+		},
 	},
 	config = function()
-		-- import lspconfig plugin
 		local lspconfig = require("lspconfig")
-
-		-- import mason_lspconfig plugin
-		local mason_lspconfig = require("mason-lspconfig")
-
-		-- import cmp-nvim-lsp plugin
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
 		local keymap = vim.keymap -- for conciseness
 
 		vim.api.nvim_create_autocmd("LspAttach", {
@@ -45,7 +44,7 @@ return {
 				keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts) -- smart rename
 
 				opts.desc = "Show buffer diagnostics"
-				keymap.set("n", "<leader>P", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+				keymap.set("n", "<leader>P", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show diagnostics for file
 
 				opts.desc = "Show line diagnostics"
 				keymap.set("n", "<leader>p", vim.diagnostic.open_float, opts) -- show diagnostics for line
@@ -68,20 +67,30 @@ return {
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
 		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
+		-- Configure LSP servers manually (since we're not using mason-lspconfig)
+		local servers = {
+			"cssls",
+			"ts_ls",
+			"html",
+			"gopls",
+			"ruby_lsp",
+			"tailwindcss",
+			"graphql",
+			"lua_ls",
+		}
+
+		for _, server in ipairs(servers) do
+			if lspconfig[server] then
+				lspconfig[server].setup({
 					capabilities = capabilities,
 				})
-			end,
-		})
+			end
+		end
 	end,
 }
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,59 +1,54 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		"jay-babu/mason-nvim-dap.nvim",
-		"mfussenegger/nvim-dap",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
-
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
-
-		local mason_tool_installer = require("mason-tool-installer")
-		local mason_dap = require("mason-nvim-dap")
-
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
 				},
-			},
-		})
-
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"cssls",
-				"gopls",
-				"graphql",
-				"html",
-				"lua_ls",
-				"ruby_lsp",
-				"tailwindcss",
-				"ts_ls",
-			},
-		})
-
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"eslint_d",
-				"prettier", -- prettier formatter
-				"rubocop",
-				"stylua", -- lua formatter
-			},
-		})
-
-		mason_dap.setup({
-			ensure_installed = { "go", "node", "delve" }, -- Example debuggers
-			automatic_setup = true,
-			automatic_installation = true,
-		})
-	end,
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					-- LSP servers
+					"css-lsp",
+					"gopls",
+					"graphql-language-service-cli",
+					"html-lsp",
+					"lua-language-server",
+					"ruby-lsp",
+					"tailwindcss-language-server",
+					"typescript-language-server",
+					-- Formatters and linters
+					"eslint_d",
+					"prettier",
+					"rubocop",
+					"stylua",
+				},
+			})
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		config = function()
+			require("mason-nvim-dap").setup({
+				ensure_installed = { "go", "node", "delve" },
+				automatic_setup = true,
+				automatic_installation = true,
+			})
+		end,
+	},
 }
