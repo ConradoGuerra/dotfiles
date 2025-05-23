7da71e4d95f642d56a9bec78d38bab442c0f19b4
conrado.guerra <conrado.guerra@ilek.fr>
2025-05-23 17:52:45 +0200

    Update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,29 +5,6 @@ return {
 		local lualine = require("lualine")
 		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-		-- Custom component for search count
-		local search_count = {
-			function()
-				local search = require("noice").api.status.search.get()
-				return search and search or ""
-			end,
-			cond = function()
-				return package.loaded["noice"] and require("noice").api.status.search.has()
-			end,
-			color = { fg = "#7dcfff" }, -- Customize color as needed
-		}
-
-		-- Get noice mode (replaces the deprecated statusline.mode.get)
-		local noice_mode = {
-			function()
-				return require("noice").api.status.mode.get()
-			end,
-			cond = function()
-				return package.loaded["noice"] and require("noice").api.status.mode.has()
-			end,
-			color = { fg = "#ff9e64" },
-		}
-
 		lualine.setup({
 			sections = {
 				lualine_c = {
@@ -37,15 +14,11 @@ return {
 					},
 				},
 				lualine_x = {
-					search_count, -- Add search count component
-					noice_mode, -- Using the updated API
 					{
 						lazy_status.updates,
 						cond = lazy_status.has_updates,
 						color = { fg = "#ff9e64" },
 					},
-					{ "encoding" },
-					{ "fileformat" },
 					{ "filetype" },
 				},
 			},
