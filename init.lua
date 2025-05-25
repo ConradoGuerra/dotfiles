a198408dcd681d0620553c2a2c95ad1c850e805c
conrado.guerra <conrado.guerra@ilek.fr>
2025-05-25 19:24:26 +0200

    Remove noice

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 require("config")
 require("config.lazy")
+require('nvim-web-devicons').setup({
+  override = {
+    css = {
+icon = "",
+      color = "#2965CC",  -- Match your theme's blue color
+      name = "CSS"
+    }
+  }
+})
