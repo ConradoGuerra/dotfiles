da12e95a436edcd9a5e845e840b88e63548b800e
conrado.guerra <conrado.guerra@ilek.fr>
2025-05-30 19:27:28 +0200

    Remove history for undo

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,31 +15,26 @@ opt.wrap = true
 opt.ignorecase = true
 opt.smartcase = true
 
--- turn on termguicolors for tokyonight colorscheme to work
--- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.background = "dark"
+opt.signcolumn = "yes"
 
--- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
--- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
--- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
-vim.o.undofile = true -- Save undo history (default: false)
+vim.o.undofile = false -- Save undo history (default: false)
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = "IncSearch", -- Highlight group (e.g., "Visual", "ErrorMsg")
-      timeout = 250,         -- Duration in milliseconds
-      on_visual = true,      -- Highlight in visual mode
-    })
-  end,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch", -- Highlight group (e.g., "Visual", "ErrorMsg")
+			timeout = 250, -- Duration in milliseconds
+			on_visual = true, -- Highlight in visual mode
+		})
+	end,
 })
